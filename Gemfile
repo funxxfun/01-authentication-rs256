3f9cc1babc06f3ddd1c710480dc455e9fa7ade6e
funxxfun <23shirasawa@gmail.com>
2024-05-15 23:41:38 +0900

    環境変数の追加

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'pry-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'sqlite3', '~> 1.4'
+
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
   get 'api/public' => 'public#public'
   get 'api/private' => 'private#private'
   get 'api/private-scoped' => 'private#private_scoped'
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
 end
